1d2c8b60226664c7cc7eb8509fbc7d6729cc68ec
HÜMEYRA ÇİMEN <75569106+hmyrcmn@users.noreply.github.com>
2024-05-30 21:06:54 +0300

    ns-allinone-3.41 files scratch file

---
+++ scratch/hmyr/build/CMakeFiles/hmyr.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hmyr.dir/main.cc.o"
+  "CMakeFiles/hmyr.dir/main.cc.o.d"
+  "hmyr"
+  "hmyr.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hmyr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ scratch/hmyr/build/CMakeFiles/hmyr.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for hmyr.
+# This may be replaced when dependencies are built.
